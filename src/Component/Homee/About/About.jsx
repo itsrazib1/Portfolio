04527e80caf6a9d64d97204afd2a87e7ada3fe51
itsrazib1 <itsrazib1@gmail.com>
2023-10-06 12:57:56 +0600

    added

--- src/Component/Homee/About/About.jsx
+++ src/Component/Homee/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <div className=' bg-white text-black bg-opacity-50 p-4 rounded-lg'>
       <h1 className="text-5xl font-bold">ABOUT ME</h1>
       <h3 className="py-6 text-3xl">A dedicated Front-end Developer <br />
-based in Belgrade, Serbia </h3>
+ </h3>
       <p>MERN Stack Developer with expertise in creating dynamic and responsive web applications. Proficient in JavaScript, React, Node.js, and MongoDB. Strong problem-solving skills and a passion for delivering high-quality code. Dedicated to continuous learning and staying updated with the latest industry trends.</p>
     </div>
   </div>
--- src/Component/Homee/Banner/Banner.jsx
+++ src/Component/Homee/Banner/Banner.jsx
@@ -1,106 +1,89 @@
-import linkdin from "../../../../public/img/640px-LinkedIn_logo_initials.png";
-import github from "../../../../public/img/github.png";
-import html5 from "../../../../public/img/html5.png";
-import css3 from "../../../../public/img/css3.png";
-import javascript from "../../../../public/img/javascript.png";
-import react from "../../../../public/img/react.png";
-import tailwind from "../../../../public/img/tailwind.png";
-import firebase from "../../../../public/img/firebase.png";
-import bootstrap from "../../../../public/img/bootstrap.jpg";
-import MongoDB from "../../../../public/img/mongodb.jpg";
-import razib from "../../../../public/img/razib.jpg";
+// import linkdin from "../../../../public/img/640px-LinkedIn_logo_initials.png";
+// import github from "../../../../public/img/github.png";
+import razib from "../../../../public/img/profile.png";
 import About from "../About/About";
 import Project from "../Project/Project";
 import Contract from "../Contract/Contract";
-import { FaDownload } from "react-icons/fa";
-import Typewriter from "../About/Typewriter";
 import pdf from "/resume.pdf";
-
+import './Banner.css'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { TypeAnimation } from 'react-type-animation';
+import { Link } from "react-router-dom";
+import Experience from "./Experience";
+import MyServices from "./MyServices";
 const Banner = () => {
  
-   const texts = ["WEB DEVELOPER", "MERN-STACK DEVELOPER", "REACT DEVELOPER"];
+  //  const texts = ["WEB DEVELOPER", "MERN-STACK DEVELOPER", "REACT DEVELOPER"];
    
   return (
     
     <>
       <div className="pt-20">
         <div className="grid  grid-cols-1 md:grid-cols-2 justify-evenly md:px-8 md:py-20 sm:px-4 sm:py-8">
-          <div className="flex bg-white ps-10 text-black bg-opacity-50  rounded-lg flex-col md:ms-48 justify-center md:justify-start sm:p-5 md:p-10 pb-5">
-            <h1 className="text-4xl sm:text-6xl md:text-6xl font-bold mt-10">
-            <div style={{ textAlign: 'center' }}>
-        <Typewriter texts={texts} />
-      </div>
-            </h1>
-            <p className="mt-4 md:mt-8 text-xl">
-              Hi, I am KHAYRUL KOBER RAZIB. <br />I am a MERN-STACK DEVELOPER .
-            </p>
-            <div className="flex mt-8 sm:mt-10 gap-5">
-              <a href="https://www.linkedin.com/in/kkoberrazib/">
-                <img
-                  className="w-7 rounded-full sm:w-10"
-                  src={linkdin}
-                  alt="LinkedIn"
-                />
-              </a>
-              <a href="https://github.com/itsrazib1">
-                <img
-                  className="w-8 sm:w-10 rounded-full"
-                  src={github}
-                  alt="GitHub"
-                />
-              </a>
-              <a href={pdf} target="_blank" rel="noreferrer"  className="bg-orange-400 px-5 py-1 rounded-lg" ><FaDownload className="inline-block  text-white bg-cyan-600"></FaDownload>{" "}
-                Download Resume</a>
-             
-            </div>
-          </div>
+        <div data-aos="fade-up" className="text-center bg-slate-900 flex flex-col justify-center px-2 md:px-12">
+                        <h5 className="text-1xl sm:text-2xl font-bold text-blue-400">
+                            <span className='text-1xl sm:text-2xl font-bold text-white'> Hi 👋 I m KHAYRUL KOBER RAZIB </span><br />
+                            <span className='text-1xl text-white '> As a </span>
+                            <br />
+
+                            <TypeAnimation className=' text-2xl md:text-3xl'
+                                sequence={[
+                                    'MERN Stack Developer',
+                                    1000,
+                                    'Font-end Developer',
+                                    1000,
+                                    'Web Developer',
+                                    1000
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                // style={{ fontSize: '2rem', display: 'inline-block' }}
+                                repeat={Infinity}
+                            />
+
+
+                        </h5>
+                        <p className="text-gray-500 py-4 px-8 text-xl">
+                            I am dedicated . I have a strong problem_solving-mindset, , great teamwork abilities, a love for learning, keen attention to detail, and a strong commitment to my work..
+                        </p>
+
+                        <div className='flex py-3 px-4'>
+                            <div data-aos="fade-right">
+                                <Link
+                                    to="contract"
+                                    className="group text-xs text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+                                >
+                                    Contract Me
+                                    <span className="group-hover:rotate-90 duration-300">
+                                        <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                                    </span>
+                                </Link>
+                            </div>
+                            <div data-aos="fade-right">
+                              
+                                <a href={pdf}
+                                    className="group text-xs text-white w-fit px-4 py-3 my-2  flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer mx-6"
+                                >
+                                    Download Resume
+                                    <span className="group-hover:rotate-90 duration-300">
+                                        <MdOutlineKeyboardArrowRight size={25} />
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
           <div className="flex justify-center items-center mt-8 md:mt-0">
             <img
               src={razib}
               alt="Profile"
-              className=" md:w-[400px] rounded-xl  "
+              className=" div md:w-[400px] rounded-xl  "
             />
           </div>
         </div>
-        <div className="mx-4 md:mx-8">
-          <div className="md:flex items-center sm:flex-col">
-            <div className="font-bold text-center mt-4 mb-4 text-2xl md:text-7xl bg-gradient-to-br from-[#68ceabe7] to-transparent px-2 md:px-7">
-              Tech Stack
-            </div>
-            <div className="flex flex-wrap items-center mx-16 space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-10 gap-4 mt-2 sm:mt-4 md:mt-0">
-              <img className="h-8 sm:h-12 md:h-16" src={html5} alt="HTML5" />
-              <img className="h-8 sm:h-12 md:h-16" src={css3} alt="CSS3" />
-              <img
-                className="h-8 sm:h-12 md:h-16"
-                src={javascript}
-                alt="JavaScript"
-              />
-              <img className="h-8 sm:h-12 md:h-16" src={react} alt="React" />
-              <img
-                className="h-8 sm:h-12 md:h-16"
-                src={tailwind}
-                alt="Tailwind CSS"
-              />
-              <img
-                className="h-8 sm:h-12 md:h-16"
-                src={firebase}
-                alt="Firebase"
-              />
-              <img
-                className="h-8 sm:h-12 md:h-16"
-                src={bootstrap}
-                alt="Firebase"
-              />
-              <img
-                className=" h-8 sm:h-12 md:h-16"
-                src={MongoDB}
-                alt="Firebase"
-              />
-            </div>
-          </div>
-        </div>
+       
       </div>
-
+<Experience></Experience>
+<MyServices></MyServices>
       <About></About>
       <Project></Project>
       <Contract></Contract>
