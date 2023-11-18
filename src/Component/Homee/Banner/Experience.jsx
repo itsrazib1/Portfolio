835e9bf9f3350790cb06598ba1bc870dd41cd907
itsrazib1 <itsrazib1@gmail.com>
2023-11-18 12:32:47 +0600

    added

--- src/Component/Homee/Banner/Experience.jsx
+++ src/Component/Homee/Banner/Experience.jsx
@@ -116,7 +116,7 @@ const Experience = () => {
                                 className={`bg-gradient-to-b to-gray-800 from-black shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
                             >
                                 <img src={src} alt="" className="w-20 mx-auto" />
-                                <p className="mt-4 text-white">{title}</p>
+                                <p className="mt-4 sm:text-base text-xs text-white">{title}</p>
                             </div>
                         ))}
                     </div>
