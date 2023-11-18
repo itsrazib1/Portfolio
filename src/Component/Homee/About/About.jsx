d81c370491636d2bb1f6c70eff7e65aa0898475b
itsrazib1 <itsrazib1@gmail.com>
2023-11-18 11:17:31 +0600

    added

--- src/Component/Homee/About/About.jsx
+++ src/Component/Homee/About/About.jsx
@@ -10,89 +10,87 @@ const About = () => {
   return (
     <div>
       <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="about-col-1">
-            <img src={img} alt="" />
-          </div>
+        <div className="container">
+          <div className="row">
+            <div className="about-col-1">
+              <img src={img} alt="" />
+            </div>
 
-          <div className="about-col-2 bg-slate-900 rounded-lg ps-10">
-            <h1 className="sub-title">About Me</h1>
-            <p>
-            MERN Stack Developer with expertise in creating dynamic and responsive web applications. Proficient in JavaScript, React, Node.js, and MongoDB. Strong problem-solving skills and a passion for delivering high-quality code. Dedicated to continuous learning and staying updated with the latest industry trends.
-            </p>
-            <div className="tab-titles">
-              <p className={`tab-links ${activeTab === 'skills' ? 'active-link' : ''}`} onClick={() => openTab('skills')}>
-                Skills
+            <div className="about-col-2 bg-slate-900 rounded-lg ps-10">
+              <h1 className="sub-title">About Me</h1>
+              <p>
+                MERN Stack Developer with expertise in creating dynamic and responsive web applications. Proficient in JavaScript, React, Node.js, and MongoDB. Strong problem-solving skills and a passion for delivering high-quality code. Dedicated to continuous learning and staying updated with the latest industry trends.
               </p>
-              <p className={`tab-links ${activeTab === 'experience' ? 'active-link' : ''}`} onClick={() => openTab('experience')}>
-                Experience
-              </p>
-              <p className={`tab-links ${activeTab === 'education' ? 'active-link' : ''}`} onClick={() => openTab('education')}>
-                Education
-              </p>
-            </div>
-            <div className={`tab-container ${activeTab === 'skills' ? 'active-tab' : ''}`} id="skills">
-              <ul>
-                <li>
-                  <span>Font-end Developer</span>
-                  <br />
-                  Designing Font-end Website Interfaces
-                </li>
-                <li>
-                  <span>Web Development</span>
-                  <br />
-                  Web app Development
-                </li>
-                <li>
-                  <span>Back-End Developer</span>
-                  <br />
-                  Designing Full-Stack Using MERN Stack.
-                </li>
-              </ul>
-            </div>
-            <div className={`tab-container ${activeTab === 'experience' ? 'active-tab' : ''}`} id="experience">
-              <ul>
-                <li>
-                  <span>2021-Current</span>
-                  <br />
-                  MERN STACK DEVELOPER
-                </li>
-                <li>
-                  <span>2021-2022</span>
-                  <br />
-                  Fullstack Development
-                </li>
-                <li>
-                  <span>2019-2021</span>
-                  <br />
-                  React Developer
-                </li>
-              </ul>
-            </div>
-            <div className={`tab-container ${activeTab === 'education' ? 'active-tab' : ''}`} id="education">
-              <ul>
-                <li>
-                  <span>2022-2026</span>
-                  <br />
-                  Studying BBA Honors
-                </li>
-                <li>
-                  <span>2021</span>
-                  <br />
-                  Completed HSC Degree
-                </li>
-                <li>
-                  <span>2019</span>
-                  <br />
-                  Completed SSC Degree
-                </li>
-              </ul>
+              <div className="tab-titles">
+                <p className={`tab-links ${activeTab === 'skills' ? 'active-link' : ''}`} onClick={() => openTab('skills')}>
+                  Skills
+                </p>
+                <p className={`tab-links ${activeTab === 'experience' ? 'active-link' : ''}`} onClick={() => openTab('experience')}>
+                  Experience
+                </p>
+                <p className={`tab-links ${activeTab === 'education' ? 'active-link' : ''}`} onClick={() => openTab('education')}>
+                  Education
+                </p>
+              </div>
+              <div className={`tab-container ${activeTab === 'skills' ? 'active-tab' : ''}`} id="skills">
+                <ul>
+                  <li>
+                    <span>Font-end Developer</span>
+                    <br />
+                    Designing Font-end Website Interfaces
+                  </li>
+                  <li>
+                    <span>Web Development</span>
+                    <br />
+                    Web app Development
+                  </li>
+                  <li>
+                    <span>Back-End Developer</span>
+                    <br />
+                    Designing Full-Stack Using MERN Stack.
+                  </li>
+                </ul>
+              </div>
+              <div className={`tab-container ${activeTab === 'experience' ? 'active-tab' : ''}`} id="experience">
+                <ul>
+                  <li>
+                    <span>2023 August-October</span>
+                    <br />
+                    Letter of Recommendation (From Programming Hero )
+                    <br />● 2 Month Team Project With Github Collaboration .
+
+                  </li>
+                  <li>
+                    <span>2023 January-July</span>
+                    <br />
+                    Complete Web Development (From Programming Hero )
+                  </li>
+                  
+                </ul>
+              </div>
+              <div className={`tab-container ${activeTab === 'education' ? 'active-tab' : ''}`} id="education">
+                <ul>
+                  <li>
+                    <span>2022-2026</span>
+                    <br />
+                    Studying BBA Honors
+                  </li>
+                  <li>
+                    <span>2021</span>
+                    <br />
+                    Completed HSC Degree
+                  </li>
+                  <li>
+                    <span>2019</span>
+                    <br />
+                    Completed SSC Degree
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
